447b5f84c398c3f42f0527df4d43a3c5f07a0d44
miwakos <goma.kabo@gmail.com>
2021-07-15 18:10:39 +0900

    rubocopによる修正（空白・改行・クォテーション）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[0-9])[a-z0-9]+\z/i, message: "is invalid. Include both letters and numbers" }
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[0-9])[a-z0-9]+\z/i,
+                                 message: 'is invalid. Include both letters and numbers' }
   with_options presence: true do
     validates :nickname
-    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ , message: "is invalid. Input full-width characters"}
-    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ , message: "is invalid. Input full-width characters"}
-    validates :last_name_kana, format: {with: /\A[ァ-ヶ一]+\z/ , message: "is invalid. Input full-width katakana characters"}
-    validates :first_name_kana, format: {with: /\A[ァ-ヶ一]+\z/ , message: "is invalid. Input full-width katakana characters"}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶ一]+\z/, message: 'is invalid. Input full-width katakana characters' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶ一]+\z/, message: 'is invalid. Input full-width katakana characters' }
     validates :user_birthday
   end
 end
