ca5c8d3a6d15b140d5837dd98f589338311d109d
miwakos <goma.kabo@gmail.com>
2021-07-16 19:08:47 +0900

    shipping_durationのアソシエーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :shipping_fee_status
   belongs_to :prefecture
+  belongs_to :shipping_duration
   has_one_attached :image
 
   with_options presence: true do
