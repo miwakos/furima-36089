03f59406e734f21265381af113528b4c87d9fd51
miwakos <goma.kabo@gmail.com>
2021-07-23 14:44:43 +0900

    エラーメッセージ日本語表示機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :description
-    with_options numericality: { other_than: 0 } do
+    with_options numericality: { other_than: 0, message: 'を選択してください' } do
       validates :category_id
       validates :condition_id
       validates :shipping_fee_status_id
